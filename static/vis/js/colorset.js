88ece65579146efca69d03c526541b4210e12f05
Maoyuan <smaoyuan@gmail.com>
2015-04-09 09:21:55 -0400

    separate color set from biset.js

---
+++ static/vis/js/colorset.js
@@ -0,0 +1,37 @@
+// a set of colors used in BiSet
+
+var colorSet = {
+	// entity normal: blue
+	entNormal: "rgba(78, 131, 232, 0.3)",
+	// entity hover
+	entHover: "rgba(228, 122, 30, 0.15)",
+
+	// entity border for normal case
+	entNormalBorder: "rgba(0, 0, 0, 1)",
+	// entity border color when mouseover
+	entMouseOverBorder: "rgba(0, 79, 173, 0.9)",
+	// entity border when selected
+	entSelBorder: "rgba(0, 0, 0, 0.6)",
+	// entity frequency (dark blue)
+	entFreColor: "rgba(22, 113, 229, 0.3)",
+
+	// normal bic frame
+	bicFrameColor: "rgba(0, 20, 20, 0.1)",
+	// bic frequency (light blue)
+	bicFreColor: "rgba(110, 207, 255, 0.4)", // "rgba(22, 113, 229, 0.2)",
+
+	// select entity to highlight bic
+	bicFrameHColor: "rgba(0, 79, 173, 0.9)", //"rgba(45, 168, 75, 0.6)",
+	// the border of the bic frame
+	// bicFrameBorderColor: "rgba(0, 0, 0, 0.6)",
+
+	// normal line (light gray)
+	lineNColor: "rgba(0, 0, 0, 0.06)",
+	// line almost hide
+	lineHDColor: "rgba(0, 0, 0, 0.04)",
+	// hover entity to show links
+	linePreHColor: "rgba(252, 30, 36, 0.28)"
+
+	// selecte entity to highlight links
+	// lineHColor: "rgba(252, 30, 36, 0.35)"
+}
